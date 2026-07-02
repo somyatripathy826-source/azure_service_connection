877794c35183a6f6dbab0859ebec94255e1d792c
Somya Tripathy <somyatripathy826@gmail.com>
2026-07-02 14:48:10 +0530

    initial commit

---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+resource_group_name = "somya_rg"
+location            = "Central India"
+acr_name            = "myuniqueacr2244"
+
+sku           = "Basic"
+admin_enabled = true
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "acr_name" {
+  type = string
+}
+
+variable "sku" {
+  type    = string
+  default = "Basic"
+}
+
+variable "admin_enabled" {
+  type    = bool
+  default = true
+}
